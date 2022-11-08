52ff4c3ede670f44af6fd66827cbb3a455117f87
MushStory <y37653@gmail.com>
2022-11-09 01:40:46 +0900

    first commit

---
+++ frontle.js
@@ -0,0 +1,126 @@
+import { f_pageLoader } from "./system/page/pageloader.js";
+const frotlePageLoader = new f_pageLoader();
+
+/**
+ * All features of Frontle are provided here
+ */
+const frontle = {
+  /**
+   * The area to manage events
+   */
+  event: {
+    /**
+     * The area to manage the event that occurs when the back button is pressed
+     */
+    back: {
+      /**
+       * Get the function to be executed when the back button is pressed
+       * @returns {function}
+       */
+      getListener: () => {
+        return frotlePageLoader.getBackListener();
+      },
+
+      /**
+       * Register a function to be executed when the back button is pressed
+       * @param {function} listener - Function to be executed when the back button is pressed
+       */
+      addListener: (listener) => {
+        frotlePageLoader.addBackListener(listener);
+      },
+
+      /**
+       * Delete the function to be executed when the back button is pressed
+       */
+      removeListener: () => {
+        frotlePageLoader.removeBackListener();
+      },
+    },
+
+    /**
+     * The area to manage the event that occurs when the forward button is pressed
+     */
+    forward: {
+      /**
+       * Get the function to be executed when the forward button is pressed
+       * @returns {function}
+       */
+      getListener: () => {
+        return frotlePageLoader.getForwardListener();
+      },
+
+      /**
+       * Register a function to be executed when the forward button is pressed
+       * @param {function} listener - Function to be executed when the forward button is pressed
+       */
+      addListener: (listener) => {
+        frotlePageLoader.addForwardListener(listener);
+      },
+
+      /**
+       * Delete the function to be executed when the forward button is pressed
+       */
+      removeListener: () => {
+        frotlePageLoader.removeForwardListener();
+      },
+    },
+  },
+
+  /**
+   * The area to provide functions of Frontle
+   */
+  util: {
+    /**
+     * Go to another page
+     * @param {string} pageName - Page name to go to
+     * @param {object} [params] - Parameters to pass
+     * @param {boolean} [displayParamsInURL] - Whether to display parameters in URL
+     */
+    pageMove: (pageName, params = {}, displayParamsInURL = true) => {
+      frotlePageLoader.pageMove(pageName, params, displayParamsInURL);
+    },
+
+    /**
+     * Go to another page. Can't go back to previous page
+     * @param {string} pageName - Page name to go to
+     * @param {object} [params] - Parameters to pass
+     * @param {boolean} [displayParamsInURL] - Whether to display parameters in URL
+     */
+    pageReplace: (pageName, params = {}, displayParamsInURL = true) => {
+      frotlePageLoader.pageReplace(pageName, params, displayParamsInURL);
+    },
+  },
+
+  /**
+   * The area to manage the functions used by the Frontle system
+   */
+  system: {
+    /**
+     * Run the Frontle system
+     * @param {function} [deviceReadyCallback] - A function that is executed when the device is ready
+     */
+    start: (deviceReadyCallback = () => {}) => {
+      let eventName = "deviceready";
+      if (String(typeof window.cordova) === "undefined") {
+        eventName = "DOMContentLoaded";
+      }
+
+      document.addEventListener(eventName, async () => {
+        await deviceReadyCallback();
+        frotlePageLoader.start();
+      });
+    },
+  },
+
+  /**
+   * The area to manage the environment
+   */
+  env: {
+    /**
+     * Values ​​that can be set with the command "frontle build --fenv <FRONTLE_ENV>"
+     */
+    /* #FRONTLE_BUILD_LINE: FRONTLE_ENV */ FRONTLE_ENV: null,
+  },
+};
+
+export { frontle };
---
+++ system/page/pageloader.js
@@ -0,0 +1,228 @@
+export class f_pageLoader {
+  static _instance = null;
+  constructor() {
+    if (f_pageLoader._instance) {
+      return f_pageLoader._instance;
+    }
+    f_pageLoader._instance = this;
+  }
+
+  // state params
+  #params = {};
+  #pageCount = 0;
+
+  // page status
+  #pageOpening = false;
+  #firstSetStateEvent = true;
+
+  // event listeners
+  #forwardListener = null;
+  #backListener = null;
+
+  // start
+  start() {
+    // parse params
+    const params = this.#parseURL();
+
+    // make state
+    let state = null;
+    if (history.state === null) {
+      state = Object.assign(params, {
+        f_p: params.f_p || "main",
+        f_u: true,
+        f_c: 0,
+      });
+    } else state = history.state;
+
+    // page open
+    this.#pageOpen("replace", state);
+  }
+
+  // page open
+  #pageOpen(stateSaveType, state) {
+    try {
+      // check page opening
+      if (this.#pageOpening) return;
+
+      // start page opening
+      this.#pageOpening = true;
+
+      // get state parameters
+      this.#params = state;
+      this.#pageCount = this.#params.f_c;
+
+      // get state URL
+      const stateURL = this.#stateURL(state);
+
+      // set state
+      stateSaveType === "push"
+        ? history.pushState(state, null, stateURL)
+        : history.replaceState(state, null, stateURL);
+
+      // set state pop and push event
+      this.#setStateEvent();
+
+      // get page path name
+      const pagePathName = this.#params.f_p
+        .replace(/[^a-zA-Z0-9_]/gi, "")
+        .toLowerCase();
+
+      import(`../../../app/${pagePathName}/${pagePathName}.js`).then(
+        (_main) => {
+          // reset page
+          this.#pageReset();
+
+          // stop page opening
+          this.#pageOpening = false;
+
+          // filter params
+          let filteredParams = JSON.parse(JSON.stringify(this.#params));
+          if (filteredParams.f_u !== undefined) delete filteredParams.f_u;
+          if (filteredParams.f_c !== undefined) delete filteredParams.f_c;
+
+          // start page
+          _main.default(filteredParams);
+        }
+      );
+    } catch (e) {
+      console.log("page not found: " + JSON.stringify(e));
+    }
+  }
+
+  // page change by state
+  #setStateEvent() {
+    // loading once
+    if (this.#firstSetStateEvent === false) return;
+    this.#firstSetStateEvent = false;
+
+    window.onpopstate = (event) => {
+      // get state
+      const state = event.state;
+
+      // get changed page count
+      const changedPageCount = Number(state.f_c);
+
+      // if push state
+      if (changedPageCount > this.#pageCount) this.#pushState(state);
+      // if pop state
+      else if (changedPageCount < this.#pageCount) this.#popState(state);
+    };
+  }
+  #pushState(state) {
+    if (this.#pageOpening) history.back();
+    else {
+      if (this.#forwardListener === null) this.#pageOpen("replace", state);
+      else {
+        history.back();
+        this.#forwardListener();
+      }
+    }
+  }
+  #popState(state) {
+    if (this.#pageOpening) history.forward();
+    else {
+      if (this.#backListener === null) this.#pageOpen("replace", state);
+      else {
+        history.forward();
+        this.#backListener();
+      }
+    }
+  }
+
+  // page change by util
+  pageMove(pageName, params = {}, displayParamsInURL = true) {
+    // make state
+    const state = Object.assign(params, {
+      f_p: pageName,
+      f_u: displayParamsInURL,
+      f_c: this.#pageCount + 1,
+    });
+
+    // page open
+    this.#pageOpen("push", state);
+  }
+  pageReplace(pageName, params = {}, displayParamsInURL = true) {
+    // make state
+    const state = Object.assign(params, {
+      f_p: pageName,
+      f_u: displayParamsInURL,
+      f_c: this.#pageCount,
+    });
+
+    // page open
+    this.#pageOpen("replace", state);
+  }
+
+  // util
+  #parseURL() {
+    let params = {};
+    window.location.search.replace(
+      /[?&]+([^=&]+)=([^&]*)/gi,
+      function (str, key, value) {
+        params[decodeURIComponent(key)] = decodeURIComponent(value);
+      }
+    );
+
+    return params;
+  }
+  #stateURL(params) {
+    let urlParams = "";
+    if (Boolean(params.f_u) === true)
+      urlParams = this.#paramsToURLString(params);
+
+    return `${location.protocol}//${location.host}${location.pathname}/../index.html?f_p=${params.f_p}${urlParams}`;
+  }
+  #paramsToURLString(params = {}) {
+    let urlParams = "";
+
+    const keys = Object.keys(params);
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      const value = params[key];
+
+      switch (key.toLocaleLowerCase()) {
+        case "f_p":
+        case "f_u":
+        case "f_c":
+          continue;
+      }
+
+      urlParams += `&${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+    }
+
+    return urlParams;
+  }
+  #pageReset() {
+    // reset event
+    this.removeForwardListener();
+    this.removeBackListener();
+
+    // reset scroll
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+
+  // event
+  getBackListener() {
+    return this.#backListener;
+  }
+  addBackListener(listener = () => {}) {
+    this.#backListener = listener;
+  }
+  removeBackListener() {
+    this.#backListener = null;
+  }
+
+  getForwardListener() {
+    return this.#forwardListener;
+  }
+  addForwardListener(listener = () => {}) {
+    this.#forwardListener = listener;
+  }
+  removeForwardListener() {
+    this.#forwardListener = null;
+  }
+}
